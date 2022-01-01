afcec5e3be1b2e9c6c99de078bad066f3c485114
Aliaksei Huivan <a.huivan@godeltech.com>
2022-01-01 22:23:44 +0300

    change file

--- GitFile.cs
+++ GitFile.cs
@@ -6,7 +6,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using System.Collections.Generic;
 using System.Linq;
-using System.Sofology.Tasks;
+using System.Sofology.Taskds;
 
 namespace CodeSecurityMonitor.Logic
 {
